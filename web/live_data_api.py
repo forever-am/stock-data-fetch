d343d518edd1b71bbc263b89a61f97e447bb3b5d
Jean-Daniel Borowy <jeandaniel.borowy@gmail.com>
2017-11-10 10:34:41 +0100

    [google-realtime-reader] Add unit test to check the realtime feature

---
+++ web/live_data_api.py
@@ -0,0 +1,6 @@
+"""
+This file contains the api to fetch realtime price of a given intrument
+"""
+
+def fetch_live_data(instrument):
+    raise NotImplementedError("Live data is no implemented")
--- web/tests/test_api.py
+++ web/tests/test_api.py
@@ -4,19 +4,20 @@ from unittest import TestCase
 import mock
 import pandas as pd
 import pandas.util.testing as pdt
+from datetime import datetime
 
 from .. import api
+from ..live_data_api import fetch_live_data
 
 TEST_DIR = path.dirname(__file__)
 
 
-def _read_time_series_csv(filename):
+def _read_data_frame_csv(filename):
     return pd.read_csv(filename, parse_dates=True, index_col=0)
-
 
 def web_reader(ticker, *args, **kwargs):
     filename = path.join(TEST_DIR, "mock-stock-data", ticker + ".csv")
-    return _read_time_series_csv(filename)
+    return _read_data_frame_csv(filename)
 
 
 @mock.patch(api.__name__ + ".mkdir")
@@ -42,13 +43,26 @@ class DataReaderTest(TestCase):
 
     def test_data_reader(self):
         ticker = "GOOG"
-        end = "2017-11-03"
+        end = "2017-11-02"
         df = api.data_reader(ticker, end=end)
         self.m_web_reader.assert_called_with(
             ticker, "yahoo", start="1926-01-01", end=end
         )
-        expected = web_reader(ticker)
+        expected = web_reader(ticker)[:end]
         pdt.assert_frame_equal(df, expected)
+
+    def test_realtime_data_reader(self):
+        """
+        Test if the dataframe contains realtime price in "Adj close" for the
+        current day when end=None
+        """
+        ticker = "GOOG"
+        end =  None
+        df = api.data_reader(ticker, end=end)
+        today = str(datetime.now().date())
+        pdt.assert_almost_equal(df.ix[today]["Adj Close"],
+                                fetch_live_data(ticker))
+
 
     @classmethod
     def tearDownClass(cls):
